47f1cea2530f61a142e07d3c29bc3a34eab61db5
jgudhlanga <james@propay.co.za>
2018-10-18 19:24:56 +0200

    add and delete quote, progress bar functionality

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container">
+    <app-header :quotes-count="quotes.length" :max-quotes="maxQuotes"></app-header>
     <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="quoteDeleted"></app-quote-grid>
     <div class="row">
       <div class="col mt-3">
         <div class="alert alert-info text-center" role="alert">
@@ -15,6 +16,7 @@
 <script>
   import QuoteGrid from './components/QuotesGrid';
   import NewQuote from './components/NewQuote';
+  import Header from './components/Header';
 export default {
   name: 'app',
   data () {
@@ -27,12 +29,20 @@ export default {
   },
   methods: {
     newQuote(quote) {
+      if (this.quotes.length === this.maxQuotes) {
+        alert('The maximum quotes reached');
+        return;
+      }
        this.quotes.push(quote);
+    },
+    quoteDeleted(index) {
+       this.quotes.splice(index,  1);
     }
   },
   components: {
     'app-quote-grid': QuoteGrid,
-    'app-new-quote': NewQuote
+    'app-new-quote': NewQuote,
+    'app-header': Header,
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row my-3">
+       <div class="col-sm-12">
+         <h3>Quotes Added</h3>
+         <div class="progress">
+           <div class="progress-bar" role="progressbar" :style="barWidth" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+              {{ quotesCount }} / {{ maxQuotes }}
+           </div>
+         </div>
+       </div>
+      <hr>
+    </div>
+</template>
+<script>
+  export default {
+    props: ['quotesCount', 'maxQuotes'],
+    computed: {
+      barWidth() {
+        return {
+          width: (this.quotesCount / this.maxQuotes) * 100 + '%'
+        }
+      }
+    }
+  }
+</script>
+
